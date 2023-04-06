90eb38497accb020083efcc7a0fd466baa72b73f
ShayanTheNerd <sh.zamani384@gmail.com>
2023-04-07 00:04:35 +0330

    Fixed path to 'main.js' file in assets folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './main.js'],
+	content: ['./index.html', './assets/main.js'],
 	theme: {
 		extend: {
 			screens: {
